5d49fd79a59ce6eb1e52cc92c20fb3c85a7fe2ed
Vishnu Bhasker <vbsoccer22@gmail.com>
2018-05-10 18:10:41 -0400

    I made the background look kinda like grass and made the platform rotate randomly

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class Platform : MonoBehaviour {
 
 	// Use this for initialization
@@ -12,9 +11,18 @@ public class Platform : MonoBehaviour {
     public Text gameEnd;
     private float timeLeft = 5;
     public Text timeText;
+    //private Rigidbody rb;
     public int count=0;
+    private int xRotate, yRotate,zRotate;
+
     // Update is called once per frame
     void Update () {
+        xRotate = Random.Range(-2, 2);
+        yRotate = Random.Range(-2, 2);
+        zRotate = Random.Range(-2, 2);
+        transform.Rotate(xRotate*Time.deltaTime, 0, 0);
+        transform.Rotate(0, yRotate*Time.deltaTime, 0);
+        transform.Rotate(0, 0, zRotate*Time.deltaTime);
         if (!(gameEnd.text == "RED WINS!" || gameEnd.text == "BLUE WINS!"))
         {
             timeLeft -= Time.deltaTime;
@@ -26,7 +34,7 @@ public class Platform : MonoBehaviour {
             if (timeLeft <= 0)
             {
                 timeLeft = 10;
-                transform.localScale *= .8f;
+                transform.localScale *= .9f;
                 count++;
             }
         }
